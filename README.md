9264c367a3769462e163531648537549b5d143bc
Hassan Raza <hassanrsiddiqi@gmail.com>
2023-11-15 00:44:54 +0500

    Add example

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ This is backed by rust (vtracer) using rustler. You need to install [rust](https
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `svg_tracing` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `svg_tracing` to your list of dependencies in `mix.exs`:
 
 ```elixir
 you need install rust 
@@ -19,16 +18,26 @@ end
 
 iex(1)> SvgTracing.trace("priv/static/ikea-sofa.png", "priv/static/ikea-sofa.svg")
 {:ok, {}}
-
-#### Input Image:
-![input_image](/priv/static/ikea-sofa.png)
-
-#### Output SVG:
-![input_image](/priv/static/ikea-sofa.svg)
-
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/svg_tracing>.
+### Examples
 
+#### Input Image:
+![input_image](/priv/static/example1.png)
+
+#### Output SVG:
+![input_image](/priv/static/example1.svg)
+
+
+#### Input Image:
+![input_image](/priv/static/example2.jpg)
+
+#### Output SVG:
+![input_image](/priv/static/example2.svg)
+
+
+#### Input Image:
+![input_image](/priv/static/example3.jpg)
+
+#### Output SVG:
+![input_image](/priv/static/example3.svg)
--- mix.exs
+++ mix.exs
@@ -1,24 +1,30 @@
 defmodule SvgTracing.MixProject do
   use Mix.Project
+  @app :svg_tracing
   @source_url "https://github.com/graydientAI/svg_tracing"
 
   def project do
     [
-      app: :svg_tracing,
+      app: @app,
       name: "svg_tracing",
       description: "Elixir Vector Graphics Converter",
       version: "0.1.4",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      compilers: [:elixir_make | Mix.compilers()],
+      make_targets: ["all"],
+      make_clean: ["clean"],
       deps: deps(),
-      package: package()
+      package: package(),
+      releases: [{@app, release()}]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {SvgTracingScript, []}
     ]
   end
 
@@ -26,7 +32,20 @@ defmodule SvgTracing.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.29.1"},
+      {:elixir_make, "~> 0.6.1"},
+      {:bakeware, "~> 0.2.4", runtime: false},
+      # {:bakeware, path: "../..", runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      quiet: true,
+      steps: [:assemble, &Bakeware.assemble/1],
+      strip_beams: Mix.env() == :prod
     ]
   end
 
